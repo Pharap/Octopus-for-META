95c481f17652183a2573e687b43913e396d3bb14
Pharap <2933055+Pharap@users.noreply.github.com>
2020-03-03 20:14:00 +0000

    Replace HD_Tools.h with the version from Parachute

--- HD_Tools.h
+++ HD_Tools.h
@@ -1,6 +1,3 @@
-#ifndef HD_TOOLS
-#define HD_TOOLS
-
 #include "Global.h"
 #include "Sprites.h"
 #include "Pictures.h"
@@ -11,14 +8,15 @@
 
 namespace Gamebuino_Meta {
 
-#define DMA_DESC_COUNT 3
-extern volatile uint32_t dma_desc_free_count;
+  #define DMA_DESC_COUNT 3
+  extern volatile uint32_t dma_desc_free_count;
 
-static inline void wait_for_transfers_done(void) {
-  while (dma_desc_free_count < DMA_DESC_COUNT);
+  static inline void wait_for_transfers_done(void) {
+    while (dma_desc_free_count < DMA_DESC_COUNT);
+  }
+
+  static SPISettings tftSPISettings = SPISettings(24000000, MSBFIRST, SPI_MODE0);
 }
-static SPISettings tftSPISettings = SPISettings(24000000, MSBFIRST, SPI_MODE0);
-};
 
 // rendering buffers
 uint16_t buffer1[screenWidth * sliceHeight];
@@ -28,25 +26,33 @@ uint16_t buffer2[screenWidth * sliceHeight];
 bool drawPending = false;
 
 
-void drawSprite(Sprite sprite, uint8_t sliceY, uint16_t* buffer) {
+void drawSprite(Sprite sprite, uint8_t sliceY, uint16_t * buffer) {
+  // Precalculate common subexpressions
+  uint16_t spriteEnd = (sprite.y + sprite.h);
+  uint16_t sliceEnd = (sliceY + sliceHeight);
+
   // Check if sprite has one part to show on the current slice
-  if (sliceY < sprite.y + sprite.h && sprite.y < sliceY + sliceHeight) {
-    // Determines the boundaries of the sprite surface within the current slice
-    uint8_t  xMin = sprite.x;
-    uint8_t  xmax = sprite.x + sprite.w - 1;
-    uint8_t  yMin = sprite.y < sliceY ? sliceY : sprite.y;
-    uint8_t  yMax = sprite.y + sprite.h >= sliceY + sliceHeight ? sliceY + sliceHeight - 1 : sprite.y + sprite.h - 1;
-    uint8_t  px, py;
-    uint16_t color;
+  if ((sliceY < spriteEnd) && (sprite.y < sliceEnd)) {
+    // Determine the boundaries of the sprite surface within the current slice
+    uint8_t xMin = sprite.x;
+    uint8_t xmax = ((sprite.x + sprite.w) - 1);
+    uint8_t yMin = (sliceY > sprite.y) ? sliceY : sprite.y;
+    uint8_t yMax = (sliceEnd <= spriteEnd) ? (sliceEnd - 1) : (spriteEnd - 1);
+
     // Display the sprite pixels to be drawn
-    for (py = yMin; py <= yMax; py++) {
-      for (px = xMin; px <= xmax; px++) {
-        // Picks the pixel color from the spritesheet
-        color = spritesheet[px + py * screenWidth];
-        // If color is the color selected for the Sprite
-        if (color == sprite.color) {
+    for (uint8_t py = yMin; py <= yMax; ++py) {
+      for (uint8_t px = xMin; px <= xmax; ++px) {
+        // Select the spritesheet
+        const uint16_t * spritesheet = (sprite.spritesheet == idSpritesheetA) ? spritesheetA : spritesheetB;
+
+        // Pick the pixel color from the spritesheet
+        uint16_t color = spritesheet[px + (py * screenWidth)];
+
+        // It colo is different from the transparency color
+        if (color != transColor) {
           // Copies the color code into the rendering buffer
-          buffer[px + (py - sliceY) * screenWidth] = sBlack;
+          size_t index = (px + ((py - sliceY) * screenWidth));
+          buffer[index] = color;
         }
       }
     }
@@ -54,23 +60,27 @@ void drawSprite(Sprite sprite, uint8_t sliceY, uint16_t* buffer) {
 }
 
 
-void drawText(Sprite sprite, uint8_t sliceY, uint16_t* buffer, uint8_t x, uint8_t y) {
-  if (sliceY < y + sprite.h && y < sliceY + sliceHeight) {
+void drawText(Sprite sprite, uint8_t sliceY, uint16_t * buffer, uint8_t x, uint8_t y) {
+  // Precalculate common subexpressions
+  uint16_t spriteEnd = (y + sprite.h);
+  uint16_t sliceEnd = (sliceY + sliceHeight);
+
+  if ((sliceY < spriteEnd) && (y < sliceEnd)) {
     uint8_t xMin = x;
-    uint8_t xmax = x + sprite.w - 1;
-    uint8_t yMin = y < sliceY ? sliceY : y;
-    uint8_t yMax = y + sprite.h >= sliceY + sliceHeight ? sliceY + sliceHeight - 1 : y + sprite.h - 1;
+    uint8_t xmax = (x + sprite.w - 1);
+    uint8_t yMin = (y < sliceY) ? sliceY : y;
+    uint8_t yMax = (spriteEnd >= sliceEnd) ? (sliceEnd - 1) : (spriteEnd - 1);
 
-    uint8_t  px, py, sx, sy;
-    uint16_t color;
+    for (uint8_t py = yMin; py <= yMax; ++py) {
+      uint8_t sy = (py - (y + sprite.y));
 
-    for (py = yMin; py <= yMax; ++py) {
-      sy = py - y + sprite.y;
-      for (px = xMin; px <= xmax; ++px) {
-        sx = px - xMin + sprite.x;
-        color = spritesheet[sx + sy * screenWidth];
-        if (color == sRed) {
-          buffer[px + (py - sliceY) * screenWidth] = sBlack;
+      for (uint8_t px = xMin; px <= xmax; ++px) {
+        uint8_t sx = (px - (xMin + sprite.x));
+        uint16_t color = spritesheetA[sx + (sy * screenWidth)];
+
+        if (color != transColor) {
+          size_t index = (px + ((py - sliceY) * screenWidth));
+          buffer[index] = color;
         }
       }
     }
@@ -78,38 +88,45 @@ void drawText(Sprite sprite, uint8_t sliceY, uint16_t* buffer, uint8_t x, uint8_
 }
 
 
-void drawScore(uint16_t displayScore, uint8_t sliceY, uint16_t* buffer) {
-  // we check first of all that the intersection between
+void drawScore(uint16_t displayScore, uint8_t sliceY, uint16_t * buffer) {
+  // Check of all that the intersection between
   // the sprite and the current slice is not empty
-  if (sliceY < 20 && sliceY + sliceHeight > 11 ) {
-    // determines the boundaries of the sprite surface within the current slice
-    uint8_t  xMin = 102;
-    uint8_t  yMin = (12 < sliceY) ? sliceY : 12;
-    uint8_t  yMax = (19 >= sliceY + sliceHeight) ? sliceY + sliceHeight - 1 : 18;
-    uint16_t color;
+  if ((sliceY < 20) && ((sliceY + sliceHeight) > 11)) {
+    // Determine the boundaries of the sprite surface within the current slice
+    uint8_t  xMin = 12;
+    uint8_t  yMin = (sliceY > 12) ? sliceY : 12;
+    uint8_t  yMax = ((sliceY + sliceHeight) <= 19) ? ((sliceY + sliceHeight) - 1) : 18;
     uint16_t remainder = displayScore;
 
     // Draw each digit of the score
-    for (uint16_t divisor = 1000; divisor > 0; divisor /= 10)     {  // The limit is 4 because 10000 is pow(10, 4)
+    // The loop has 4 iterations because 10000 is pow(10, 4)
+    for (uint16_t divisor = 10000; divisor > 0; divisor /= 10) {
+      // The quotient is the digit to be displayed
       uint16_t quotient = (remainder / divisor);
+
+      // The remainder is what's left over
       remainder = (remainder % divisor);
+
+      // Move the cursor along 6 pixels each time
       xMin += 6;
+      
       // Go through the sprite pixels to be drawn
-      for (uint8_t py = yMin; py <= yMax; ++py)   {
-        for (uint8_t px = 0; px < 4; ++px)   {
+      for (uint8_t py = yMin; py <= yMax; ++py) {
+        for (uint8_t px = 1; px <= 5; ++px) {
 
           // Calculate the colour offset
-          size_t colourIndex = (px + (6 * quotient) + ((py - 8) * screenWidth));
+          size_t colourIndex = (px + (6 * quotient) + ((py - 11) * screenWidth));
 
           // Pick the pixel colour from the spritesheet
-          uint16_t colour = spritesheet[colourIndex];
+          uint16_t colour = spritesheetA[colourIndex];
 
-          // If it is not the transparency colour
-          if (colour == sRed)     {
+          // If the colour is not the transparency colour
+          if (colour != transColor) {
             // Calculate the colour offset
             size_t bufferIndex = (xMin + px + ((py - sliceY) * screenWidth));
+
             // Copy the colour code into the rendering buffer
-            buffer[bufferIndex] = sBlack ;
+            buffer[bufferIndex] = colour;
           }
         }
       }
@@ -143,38 +160,35 @@ void waitForPreviousDraw() {
 }
 
 
-void drawBackground(const uint16_t * background, const Sprite spriteToDisplay, uint8_t spriteX, uint8_t spriteY, boolean displaySprite)
-{
-  constexpr size_t bufferSize = (sizeof(uint16_t) * screenWidth * sliceHeight);
+void drawBackground(const uint16_t * background, const Sprite spriteToDisplay, uint8_t spriteX, uint8_t spriteY, boolean displaySprite) {
+    constexpr size_t bufferSize = (sizeof(uint16_t) * screenWidth * sliceHeight);
 
-  for (uint8_t sliceIndex = 0; sliceIndex < slices; sliceIndex++)
-  {
-    // buffers are switched according to the parity of sliceIndex
-    uint16_t * buffer = (sliceIndex % 2 == 0) ? buffer1 : buffer2;
+    for (uint8_t sliceIndex = 0; sliceIndex < slices; ++sliceIndex)
+    {
+        // buffers are switched according to the parity of sliceIndex
+        uint16_t * buffer = (sliceIndex % 2 == 0) ? buffer1 : buffer2;
+        
+        // the top border of the current slice is calculated
+        uint8_t sliceY = sliceIndex * sliceHeight;
+        
+        size_t backgroundOffset = (sliceY * screenWidth);
 
-    // the top border of the current slice is calculated
-    uint8_t sliceY = sliceIndex * sliceHeight;
+        // starts by drawing the background
+        memcpy(buffer, &background[backgroundOffset], bufferSize);
 
-    size_t backgroundOffset = (sliceY * screenWidth);
+       // and finally draws the sprite if needed
+        if (displaySprite == true) drawText(spriteToDisplay, sliceY, buffer, spriteX, spriteY);
 
-    // starts by drawing the background
-    memcpy(buffer, &background[backgroundOffset], bufferSize);
+        // then we make sure that the sending of the previous buffer
+        // to the DMA controller has taken place
+        if (sliceIndex != 0)
+            waitForPreviousDraw();
+    
+        // after which we can then send the current buffer
+        customDrawBuffer(0, sliceY, buffer, screenWidth, sliceHeight);
+    }
 
-    // and finally draws the sprite if needed
-    if (displaySprite == true) drawText(spriteToDisplay, sliceY, buffer, spriteX, spriteY);
-
-    // then we make sure that the sending of the previous buffer
-    // to the DMA controller has taken place
-    if (sliceIndex != 0)
-      waitForPreviousDraw();
-
-    // after which we can then send the current buffer
-    customDrawBuffer(0, sliceY, buffer, screenWidth, sliceHeight);
-  }
-
-  // always wait until the DMA transfer is completed
-  // for the last slice before entering the next cycle
-  waitForPreviousDraw();
+    // always wait until the DMA transfer is completed
+    // for the last slice before entering the next cycle
+    waitForPreviousDraw();
 }
-
-#endif
